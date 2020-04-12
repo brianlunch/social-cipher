24f748163f4a707cecc895903bd4981cef98dee5
brianlunch <blynch5@tcd.ie>
2020-04-12 22:18:10 +0100

    Updates

---
+++ precache-manifest.626b55664b3742ca91d4c208abca32ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6138f839c8b99a4c94bc0fdc4fdf5589",
+    "url": "/social-cipher/index.html"
+  },
+  {
+    "revision": "478002daff1714e78126",
+    "url": "/social-cipher/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bb656f07d2792f492b8f",
+    "url": "/social-cipher/static/css/main.c217aeb5.chunk.css"
+  },
+  {
+    "revision": "478002daff1714e78126",
+    "url": "/social-cipher/static/js/2.3712d95c.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/social-cipher/static/js/2.3712d95c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb656f07d2792f492b8f",
+    "url": "/social-cipher/static/js/main.61ca94e8.chunk.js"
+  },
+  {
+    "revision": "bf540f598923b3729e71",
+    "url": "/social-cipher/static/js/runtime-main.846b5254.js"
+  }
+]);
